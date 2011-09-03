fd7328e34b5cd99724726703d62062dbd092466d
Kevin Kelley <dev@foos-bar.com>
2011-09-03 00:45:21 +0000

    Issue 22 - Added support for plugin open in more than one window.

--- mailsnag/src/com/foosbar/mailsnag/Activator.java
+++ mailsnag/src/com/foosbar/mailsnag/Activator.java
@@ -19,20 +19,22 @@ import com.foosbar.mailsnag.views.MessagesView;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin implements IStartup {
+public class Activator
+		extends AbstractUIPlugin
+		implements IStartup {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.foos-bar.mailsnag";
 
-	//Locale Specific Resource Bundle
-	private static final ResourceBundle BUNDLE = 
-		ResourceBundle.getBundle("i18n.Resources");
+	// Locale Specific Resource Bundle
+	private static final ResourceBundle BUNDLE =
+			ResourceBundle.getBundle("i18n.Resources");
 
 	// The shared instance
 	private static Activator plugin;
 
 	// SMTP Server
-	private Server server = new Server();
+	private final Server server = new Server();
 
 	/**
 	 * The constructor
@@ -42,70 +44,84 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framewActivatorork.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framewActivatorork
+	 * .BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	@Override
+	public void start(BundleContext context)
+			throws Exception {
 		super.start(context);
 		plugin = this;
 
-		//Start Server if:
-		//   a) View is open
-		//   b) User preference selected autostart
-		Display.getDefault().asyncExec(new Runnable(){
-            public void run() {
+		// Start Server if:
+		// a) View is open
+		// b) User preference selected autostart
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
 
-            	//Check if view is loaded
-            	MessagesView view = (MessagesView) PlatformUI
-    			.getWorkbench()
-    				.getActiveWorkbenchWindow()
-    					.getActivePage()
-    						.findView(MessagesView.ID);
+				// Check if view is loaded
+				MessagesView view = (MessagesView) PlatformUI
+						.getWorkbench()
+						.getActiveWorkbenchWindow()
+						.getActivePage()
+						.findView(MessagesView.ID);
 
-            	//If view is loaded, check user preference
-            	if(view != null) {
-	        		//Get user preferences
-	        		IPreferenceStore store = 
-	        			Activator.getDefault().getPreferenceStore();
-	        		
-	        		//Start server if the preferences indicate such.
-	        		if(store.getBoolean(PreferenceConstants.PARAM_STARTUP))
-	        			Activator.getDefault().startServer();
-            	}
-            }
+				// If view is loaded, check user preference
+				if (view != null) {
+					// Get user preferences
+					IPreferenceStore store =
+							Activator.getDefault().getPreferenceStore();
+
+					// Start server if the preferences indicate such.
+					if (store.getBoolean(PreferenceConstants.PARAM_STARTUP)) {
+						Activator.getDefault().startServer();
+					}
+				}
+			}
 		});
 	}
 
 	public void startServer() {
 		ThreadGroup tg = new ServerThreadGroup("SMTPServer");
-		new Thread(tg,server).start();
+		new Thread(tg, this.server).start();
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
-	public void stop(BundleContext context) throws Exception {
+	@Override
+	public void stop(BundleContext context)
+			throws Exception {
 		// Get Persist Preference
-		boolean persist = 
-			plugin.getPreferenceStore().getBoolean(PreferenceConstants.PARAM_PERSIST);
-		
-		// If not persisting, delete all messages
-		if(!persist)
-			MessageStore.removeAll();
+		boolean persist =
+				plugin.getPreferenceStore().getBoolean(
+						PreferenceConstants.PARAM_PERSIST);
 
-		if(server != null)
+		// If not persisting, delete all messages
+		if (!persist) {
+			MessageStore.removeAll();
+		}
+
+		if (this.server != null) {
 			Activator.getDefault().stopServer();
-		
+		}
+
 		super.stop(context);
 	}
 
 	public void stopServer() {
-		server.close();
+		this.server.close();
 	}
-	
+
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -115,12 +131,13 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 	public static ResourceBundle getResourceBundle() {
 		return BUNDLE;
 	}
-	
+
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
@@ -128,8 +145,9 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 	}
 
 	/**
-	 * Registers plugin for startup when Workbench starts
-	 * Can be overridden by user in Preferences > General > Startup
+	 * Registers plugin for startup when Workbench starts Can be overridden by
+	 * user in Preferences > General > Startup
+	 * 
 	 * @see org.eclipse.ui.IStartup#earlyStartup()
 	 */
 	public void earlyStartup() {
--- mailsnag/src/com/foosbar/mailsnag/smtp/MailHandler.java
+++ mailsnag/src/com/foosbar/mailsnag/smtp/MailHandler.java
@@ -18,161 +18,178 @@ import com.foosbar.mailsnag.util.MessageStore;
 import com.foosbar.mailsnag.views.MessagesView;
 import com.foosbar.mailsnag.views.MessagesView.ViewContentProvider;
 
-public class MailHandler extends Thread {
-	
-	private static final Pattern CMD_DATA = Pattern.compile("^DATA", Pattern.CASE_INSENSITIVE);
-	private static final Pattern CMD_GREETING = Pattern.compile("^(HELO|eHlo)\\s+?(.+?)$", Pattern.CASE_INSENSITIVE);
-	private static final Pattern CMD_NOOP = Pattern.compile("^NOOP", Pattern.CASE_INSENSITIVE);
-	private static final Pattern CMD_RSET = Pattern.compile("^RSET", Pattern.CASE_INSENSITIVE);
-	private static final Pattern CMD_VRFY = Pattern.compile("^VRFY", Pattern.CASE_INSENSITIVE);
-	private static final Pattern CMD_QUIT = Pattern.compile("^QUIT", Pattern.CASE_INSENSITIVE);
+public class MailHandler
+		extends Thread {
 
-	private static final String RSPN_HI  = "220 Welcome to MailSnag by Foos-Bar\r\n";
-	private static final String RSPN_OK  = "250 Ok\r\n"; 
+	private static final Pattern CMD_DATA = Pattern.compile("^DATA",
+			Pattern.CASE_INSENSITIVE);
+	private static final Pattern CMD_GREETING = Pattern.compile(
+			"^(HELO|eHlo)\\s+?(.+?)$", Pattern.CASE_INSENSITIVE);
+	private static final Pattern CMD_NOOP = Pattern.compile("^NOOP",
+			Pattern.CASE_INSENSITIVE);
+	private static final Pattern CMD_RSET = Pattern.compile("^RSET",
+			Pattern.CASE_INSENSITIVE);
+	private static final Pattern CMD_VRFY = Pattern.compile("^VRFY",
+			Pattern.CASE_INSENSITIVE);
+	private static final Pattern CMD_QUIT = Pattern.compile("^QUIT",
+			Pattern.CASE_INSENSITIVE);
+
+	private static final String RSPN_HI = "220 Welcome to MailSnag by Foos-Bar\r\n";
+	private static final String RSPN_OK = "250 Ok\r\n";
 	private static final String RSPN_BYE = "221 Bye\r\n";
 	private static final String RSPN_END_DATA = "354 End data with <CRLF>.<CRLF>\r\n";
-	
+
 	private static final String NEWLINE = System.getProperty("line.separator");
-	
-	private Socket socket;
-	
-	private boolean debug;
-	
-	private IPreferenceStore pStore;
-	
+
+	private final Socket socket;
+
+	private final boolean debug;
+
+	private final IPreferenceStore pStore;
+
 	public MailHandler(Socket socket) {
 		super("Email Handler Thread");
-		
+
 		this.socket = socket;
-		
-		this.pStore = 
-			Activator.getDefault().getPreferenceStore();
-		
-		this.debug = 
-			pStore.getBoolean(PreferenceConstants.PARAM_DEBUG);
+
+		this.pStore =
+				Activator.getDefault().getPreferenceStore();
+
+		this.debug =
+				this.pStore.getBoolean(PreferenceConstants.PARAM_DEBUG);
 	}
 
+	@Override
 	public void run() {
-		
+
 		try {
-			
+
 			StringBuilder msgBody = new StringBuilder();
-			
-			if(debug) {
+
+			if (this.debug) {
 				System.out.println("Incoming Message; Handler Thread Running");
 			}
-			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+			PrintWriter out = new PrintWriter(this.socket.getOutputStream(),
+					true);
 
 			respond(RSPN_HI, out);
 
-			BufferedReader in = 
-				new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader in =
+					new BufferedReader(new InputStreamReader(
+							this.socket.getInputStream()));
 
 			boolean readingData = false;
-			
-			String inputLine = null;
-			while ( (inputLine = in.readLine()) != null ) {
 
-				if(!readingData) {
-				
-					if(debug) {
+			String inputLine = null;
+			while ((inputLine = in.readLine()) != null) {
+
+				if (!readingData) {
+
+					if (this.debug) {
 						System.out.println("Client Command: " + inputLine);
 					}
 
 					// Greeting - Say Hello and move on.
 					Matcher m = CMD_GREETING.matcher(inputLine);
-					if(m.matches()) {
+					if (m.matches()) {
 						String server = m.group(2);
 						respond("250 Hello " + server.trim() + "\r\n", out);
 						continue;
 					}
 					// Greeting - Say Hello and move on.
-					//if(inputLine.startsWith(CMD_EHLO)) {
-					//	String server = inputLine.substring(CMD_EHLO.length());
-					//	respond("250 Hello " + server.trim() + "\r\n", out);
-//						respond("250-AUTH PLAIN LOGIN DIGEST-MD5 CRAM-MD5 GSSAPI", out);
-//						respond("250-AUTH=PLAIN LOGIN DIGEST-MD5 CRAM-MD5 GSSAPI", out);
-					//	continue;
-					//}
+					// if(inputLine.startsWith(CMD_EHLO)) {
+					// String server = inputLine.substring(CMD_EHLO.length());
+					// respond("250 Hello " + server.trim() + "\r\n", out);
+					// respond("250-AUTH PLAIN LOGIN DIGEST-MD5 CRAM-MD5 GSSAPI",
+					// out);
+					// respond("250-AUTH=PLAIN LOGIN DIGEST-MD5 CRAM-MD5 GSSAPI",
+					// out);
+					// continue;
+					// }
 
-					//No operation
+					// No operation
 					m = CMD_NOOP.matcher(inputLine);
-					if(m.matches()) {
+					if (m.matches()) {
 						respond(RSPN_OK, out);
 						continue;
 					}
 
-					//Reset
+					// Reset
 					m = CMD_RSET.matcher(inputLine);
-					if(m.matches()) {
+					if (m.matches()) {
 						readingData = false;
 						respond(RSPN_OK, out);
 						continue;
 					}
 
-					//Verify
+					// Verify
 					m = CMD_VRFY.matcher(inputLine);
-					if(m.matches()) {
+					if (m.matches()) {
 						respond(RSPN_OK, out);
 						continue;
 					}
 
 					// The End
 					m = CMD_QUIT.matcher(inputLine);
-					if(m.matches()) {
+					if (m.matches()) {
 						respond(RSPN_BYE, out);
-						if(debug)
+						if (this.debug) {
 							System.out.println("Closing connection");
+						}
 						break;
 					}
-	
+
 					// Start of Data - Confirm end of data string
 					m = CMD_DATA.matcher(inputLine);
-					if(m.matches()) {
+					if (m.matches()) {
 						readingData = true;
 						respond(RSPN_END_DATA, out);
 						continue;
 					}
-					
-					//Didn't match any other commands.  Keep reading.
+
+					// Didn't match any other commands. Keep reading.
 					respond(RSPN_OK, out);
 					continue;
 				}
-				//Reading the data block
+				// Reading the data block
 				else {
-					
-					if(debug) {
+
+					if (this.debug) {
 						System.out.println(inputLine);
 					}
-					
-					if(inputLine.equals(".")) {
+
+					if (inputLine.equals(".")) {
 						readingData = false;
 						respond(RSPN_OK, out);
 						continue;
 					} else {
 						// Issue 11 - I believe the trim
-						//    was implemented to fix a previous issue.
-						//msgBody.append(inputLine.trim()) 
+						// was implemented to fix a previous issue.
+						// msgBody.append(inputLine.trim())
 
 						// Write line to message body
 						msgBody.append(inputLine)
-							.append(NEWLINE);
+								.append(NEWLINE);
 						continue;
 					}
 				}
 			}
-			
-			if(msgBody.length() > 0) {
-				//Persist message
-				final Message message = MessageStore.persist(msgBody.toString());
-			
-				//Update the Content Provider
+
+			if (msgBody.length() > 0) {
+				// Persist message
+				final Message message = MessageStore
+						.persist(msgBody.toString());
+
+				// Update the Content Provider
 				Display.getDefault().asyncExec(new Runnable() {
 					public void run() {
-						MessagesView view = (MessagesView) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findView(MessagesView.ID);
-						ViewContentProvider provider = 
-							(ViewContentProvider) view.getViewer().getContentProvider();
+						MessagesView view = (MessagesView) PlatformUI
+								.getWorkbench().getActiveWorkbenchWindow()
+								.getActivePage().findView(MessagesView.ID);
+						ViewContentProvider provider =
+								(ViewContentProvider) view.getViewer()
+										.getContentProvider();
 						provider.add(message);
 					}
 				});
@@ -182,18 +199,20 @@ public class MailHandler extends Thread {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(socket != null && !socket.isClosed())
-					socket.close();
-			} catch(Exception e) {
+				if (this.socket != null && !this.socket.isClosed()) {
+					this.socket.close();
+				}
+			} catch (Exception e) {
 			}
 		}
 	}
 
 	private void respond(String response, PrintWriter writer) {
-		
-		if(debug)
+
+		if (this.debug) {
 			System.out.print("Server Response: " + response);
-		
+		}
+
 		writer.print(response);
 		writer.flush();
 	}
