3c8183fe30e8158bce9395c5b43edcf9e2c617c0
Kevin Kelley <dev@foos-bar.com>
2011-09-07 01:07:46 +0000

    Issue 23:	Wrong image path for embedded images in HTML e-mail previews

--- mailsnag/src/com/foosbar/mailsnag/editors/MessageEditor.java
+++ mailsnag/src/com/foosbar/mailsnag/editors/MessageEditor.java
@@ -1,6 +1,5 @@
 package com.foosbar.mailsnag.editors;
 
-
 import java.io.File;
 import java.text.NumberFormat;
 import java.util.Collection;
@@ -47,10 +46,12 @@ import com.foosbar.mailsnag.util.InlineFilter;
  * <li>page 2 Raw Data for inspecting the entire email stream.
  * </ul>
  */
-public class MessageEditor extends MultiPageEditorPart implements IResourceChangeListener{
+public class MessageEditor
+		extends MultiPageEditorPart
+		implements IResourceChangeListener {
 
 	public static final String ID = "com.foosbar.mailsnag.editors.MessageEditor";
-	
+
 	/* Locale Specific Resource Bundle */
 	private static final ResourceBundle BUNDLE = Activator.getResourceBundle();
 
@@ -64,7 +65,7 @@ public class MessageEditor extends MultiPageEditorPart implements IResourceChang
 		super();
 		ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
 	}
-	
+
 	/**
 	 * Creates a page/tab to display the raw email for inspection.
 	 */
@@ -73,60 +74,68 @@ public class MessageEditor extends MultiPageEditorPart implements IResourceChang
 
 		FillLayout layout = new FillLayout();
 		composite.setLayout(layout);
-		
+
 		StyledText text = new StyledText(composite, SWT.H_SCROLL | SWT.V_SCROLL);
 		text.setEditable(false);
-		text.setText(messageData.getMessage());
+		text.setText(this.messageData.getMessage());
 
 		int index = addPage(composite);
 		setPageText(index, BUNDLE.getString("editor.rawData"));
 	}
-	
+
 	/**
 	 * Creates a page/tab to display attachments.
 	 */
 	void createAttachments() {
-		
-		if(!message.hasAttachments())
-			return;
 
-		Collection<Attachment> attachments = message.getAttachments().values();
-		
+		if (!this.message.hasAttachments()) {
+			return;
+		}
+
+		Collection<Attachment> attachments = this.message.getAttachments()
+				.values();
+
 		Composite composite = new Composite(getContainer(), SWT.NONE);
 		FillLayout layout = new FillLayout();
 		composite.setLayout(layout);
-		
+
 		FormToolkit toolkit = new FormToolkit(composite.getDisplay());
 		ScrolledForm form = toolkit.createScrolledForm(composite);
 		form.setText(BUNDLE.getString("editor.attachments.title"));
-		
+
 		TableWrapLayout twlayout = new TableWrapLayout();
 		twlayout.numColumns = 4;
-		
+
 		Composite body = form.getBody();
 		body.setLayout(twlayout);
-		
+
 		int count = 0;
-		for(final Attachment attachment : attachments) {
+		for (final Attachment attachment : attachments) {
 			StyledText text = new StyledText(body, SWT.WRAP);
 			text.setEditable(false);
 			text.setText(Integer.toString(++count));
 			text.setLayoutData(new TableWrapData());
 
-			Hyperlink link = toolkit.createHyperlink(body, "Click Here", SWT.WRAP);
+			Hyperlink link = toolkit.createHyperlink(body, "Click Here",
+					SWT.WRAP);
 			link.addHyperlinkListener(new HyperlinkAdapter() {
+				@Override
 				public void linkActivated(HyperlinkEvent e) {
-					
-					//Not necessary - done when editor is created. see #createPages();
-					//MessageStore.persistAttachment(attachment,  messageData.getMessage());
-					
+
+					// Not necessary - done when editor is created. see
+					// #createPages();
+					// MessageStore.persistAttachment(attachment,
+					// messageData.getMessage());
+
 					try {
 						IDE.openEditor(
-							PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), 
-							new AttachmentEditorInput(attachment), 
-							IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID, 
-							true);
-						
+								PlatformUI.getWorkbench()
+										.getActiveWorkbenchWindow()
+										.getActivePage(),
+								new AttachmentEditorInput(attachment),
+								IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID,
+								true);
+
 					} catch (PartInitException ex) {
 						ex.printStackTrace();
 					}
@@ -141,174 +150,205 @@ public class MessageEditor extends MultiPageEditorPart implements IResourceChang
 			text.setLayoutData(new TableWrapData());
 
 			NumberFormat format = NumberFormat.getInstance();
-			
+
 			text = new StyledText(form.getBody(), SWT.WRAP);
 			text.setEditable(false);
 			text.setText(format.format(attachment.getSize()) + " bytes");
 			text.setLayoutData(new TableWrapData());
-			
+
 		}
-		
+
 		int index = addPage(composite);
 		setPageText(index, BUNDLE.getString("editor.attachments"));
 	}
-	
+
 	/**
 	 * Creates a page/tab to display the text email for preview.
 	 */
 	void createTextPage() {
 
-		if(messageData == null || !messageData.hasTextMessage())
+		if (this.messageData == null || !this.messageData.hasTextMessage()) {
 			return;
-		
+		}
+
 		Composite composite = new Composite(getContainer(), SWT.NONE);
 
 		FillLayout layout = new FillLayout();
 		composite.setLayout(layout);
-		
+
 		StyledText text = new StyledText(composite, SWT.H_SCROLL | SWT.V_SCROLL);
 		text.setEditable(false);
-		text.setText(messageData.getTextMessage());
-		
+		text.setText(this.messageData.getTextMessage());
+
 		int index = addPage(composite);
 		setPageText(index, BUNDLE.getString("editor.textFormat"));
 	}
-	
+
 	/**
 	 * Creates a page/tab to display the html email for preview.
 	 */
 	void createHtmlPage() {
-		
-		if(messageData == null || !messageData.hasHtmlMessage())
-			return;
 
-		
+		if (this.messageData == null || !this.messageData.hasHtmlMessage()) {
+			return;
+		}
+
 		try {
 
 			/*
-			File file = MessageStore.createTempHtmlFile(messageData.getHtmlMessage());
-			
-			WebBrowserEditor editor = new WebBrowserEditor();
-			WebBrowserEditorInput input = new WebBrowserEditorInput(file.toURI().toURL());
-			
-			setPageText(
-					addPage(editor, input), 
-					BUNDLE.getString("editor.htmlFormat"));
-			*/
+			 * File file =
+			 * MessageStore.createTempHtmlFile(messageData.getHtmlMessage());
+			 * 
+			 * WebBrowserEditor editor = new WebBrowserEditor();
+			 * WebBrowserEditorInput input = new
+			 * WebBrowserEditorInput(file.toURI().toURL());
+			 * 
+			 * setPageText( addPage(editor, input),
+			 * BUNDLE.getString("editor.htmlFormat"));
+			 */
 			Composite composite = new Composite(getContainer(), SWT.NONE);
 			composite.setLayout(new FillLayout());
 
-			Browser browser = new Browser(composite, SWT.H_SCROLL | SWT.V_SCROLL);
-			String filteredText = InlineFilter.filter(message, messageData.getHtmlMessage(), Activator.getDefault().getStateLocation().toString() + File.separator + message.getAttachmentDir());
+			Browser browser = new Browser(composite, SWT.H_SCROLL
+					| SWT.V_SCROLL);
+
+			StringBuilder path = new StringBuilder("file:///")
+					.append(Activator.getDefault().getStateLocation()
+							.toString())
+					.append(File.separator)
+					.append(this.message.getAttachmentDir());
+
+			System.out.println("PATH => " + path);
+
+			String filteredText = InlineFilter.filter(this.message,
+					this.messageData.getHtmlMessage(), path.toString());
+
 			System.out.println(filteredText);
 			browser.setText(filteredText);
 			browser.setCapture(true);
 
 			// Get Preferences
-			//IPreferenceStore pStore = 
-			//	Activator.getDefault().getPreferenceStore();
-			
+			// IPreferenceStore pStore =
+			// Activator.getDefault().getPreferenceStore();
+
 			// Execute Javascript?
-			//browser.setJavascriptEnabled(pStore
-			//		.getBoolean(PreferenceConstants.PARAM_JAVASCRIPT));
+			// browser.setJavascriptEnabled(pStore
+			// .getBoolean(PreferenceConstants.PARAM_JAVASCRIPT));
 
 			setPageText(
-					addPage(composite), 
+					addPage(composite),
 					BUNDLE.getString("editor.htmlFormat"));
 
 		} catch (Exception e) {
 			e.printStackTrace();
 			e.getCause().printStackTrace();
 			ErrorDialog.openError(
-				getSite().getShell(),
-				"Error creating nested text editor",
-				null,
-				null);
+					getSite().getShell(),
+					"Error creating nested text editor",
+					null,
+					null);
 		}
 	}
+
 	/**
 	 * Creates the pages of the multi-page editor.
 	 */
+	@Override
 	protected void createPages() {
-		//Create HTML View of Email if necessary
+		// Create HTML View of Email if necessary
 		createHtmlPage();
-		
-		//Create Text-only View of Email if necessary
+
+		// Create Text-only View of Email if necessary
 		createTextPage();
-		
-		//List any attachments included with Email
+
+		// List any attachments included with Email
 		createAttachments();
-		
-		//Create Raw Data View.
+
+		// Create Raw Data View.
 		createRawPage();
 	}
+
 	/**
-	 * The <code>MultiPageEditorPart</code> implementation of this 
+	 * The <code>MultiPageEditorPart</code> implementation of this
 	 * <code>IWorkbenchPart</code> method disposes all nested editors.
 	 * Subclasses may extend.
 	 */
+	@Override
 	public void dispose() {
 		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
 		super.dispose();
 	}
+
 	/**
 	 * Saves the multi-page editor's document.
 	 */
+	@Override
 	public void doSave(IProgressMonitor monitor) {
 		getEditor(0).doSave(monitor);
 	}
+
 	/**
-	 * Saves the multi-page editor's document as another file.
-	 * Also updates the text for page 0's tab, and updates this multi-page editor's input
-	 * to correspond to the nested editor's.
+	 * Saves the multi-page editor's document as another file. Also updates the
+	 * text for page 0's tab, and updates this multi-page editor's input to
+	 * correspond to the nested editor's.
 	 */
+	@Override
 	public void doSaveAs() {
 		IEditorPart editor = getEditor(0);
 		editor.doSaveAs();
 		setPageText(0, editor.getTitle());
 		setInput(editor.getEditorInput());
 	}
-	/* (non-Javadoc)
-	 * Method declared on IEditorPart
+
+	/*
+	 * (non-Javadoc) Method declared on IEditorPart
 	 */
 	public void gotoMarker(IMarker marker) {
 		setActivePage(0);
 		IDE.gotoMarker(getEditor(0), marker);
 	}
+
 	/**
 	 * The <code>MultiPageEditorExample</code> implementation of this method
 	 * checks that the input is an instance of <code>IFileEditorInput</code>.
 	 */
+	@Override
 	public void init(IEditorSite site, IEditorInput editorInput)
-		throws PartInitException {
+			throws PartInitException {
 		super.init(site, editorInput);
-		if (!(editorInput instanceof MessageEditorInput))
-			throw new PartInitException("Invalid Input: Must be MessageEditorInput");
-		
-		setSite(site); 
+		if (!(editorInput instanceof MessageEditorInput)) {
+			throw new PartInitException(
+					"Invalid Input: Must be MessageEditorInput");
+		}
+
+		setSite(site);
 		setInput(editorInput);
-		
-		this.message = 
-			((MessageEditorInput) editorInput).getMessage();
-		
-		this.messageData = 
-			((MessageEditorInput) editorInput).getMessageData();
-		
+
+		this.message =
+				((MessageEditorInput) editorInput).getMessage();
+
+		this.messageData =
+				((MessageEditorInput) editorInput).getMessageData();
+
 		setPartName(((MessageEditorInput) editorInput).getName());
 	}
-	/* (non-Javadoc)
-	 * Method declared on IEditorPart.
+
+	/*
+	 * (non-Javadoc) Method declared on IEditorPart.
 	 */
+	@Override
 	public boolean isSaveAsAllowed() {
 		return false;
 	}
 
+	@Override
 	protected void pageChange(int newPageIndex) {
 		super.pageChange(newPageIndex);
 		if (newPageIndex == 2) {
 			return; // Do nothing
 		}
 	}
+
 	/**
 	 * Closes all project files on project close.
 	 * 
@@ -316,25 +356,20 @@ public class MessageEditor extends MultiPageEditorPart implements IResourceChang
 	 */
 	public void resourceChanged(final IResourceChangeEvent event) {
 		/*
-		if (event.getType() == IResourceChangeEvent.POST_CHANGE) {
-			Display.getDefault().asyncExec(new Runnable() {
-				public void run() {
-					IWorkbenchPage[] pages = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getPages();
-					for (int i = 0; i < pages.length; i++) {
-						IEditorInput editor = getEditorInput();
-						
-						if(!(editor instanceof MessageEditorInput))
-							continue;
-						
-						MessageEditorInput mEditor = (MessageEditorInput) editor;
-						event.getResource().
-						if ( mEditor.getFile().getProject().equals(event.getResource()) ) {
-							IEditorPart editorPart = pages[i].findEditor(mEditor);
-							pages[i].closeEditor(editorPart, true);
-						}
-					}
-				}
-			});		
-		}*/
+		 * if (event.getType() == IResourceChangeEvent.POST_CHANGE) {
+		 * Display.getDefault().asyncExec(new Runnable() { public void run() {
+		 * IWorkbenchPage[] pages =
+		 * PlatformUI.getWorkbench().getActiveWorkbenchWindow().getPages(); for
+		 * (int i = 0; i < pages.length; i++) { IEditorInput editor =
+		 * getEditorInput();
+		 * 
+		 * if(!(editor instanceof MessageEditorInput)) continue;
+		 * 
+		 * MessageEditorInput mEditor = (MessageEditorInput) editor;
+		 * event.getResource(). if (
+		 * mEditor.getFile().getProject().equals(event.getResource()) ) {
+		 * IEditorPart editorPart = pages[i].findEditor(mEditor);
+		 * pages[i].closeEditor(editorPart, true); } } } }); }
+		 */
 	}
 }
--- mailsnag/src/com/foosbar/mailsnag/util/InlineFilter.java
+++ mailsnag/src/com/foosbar/mailsnag/util/InlineFilter.java
@@ -8,25 +8,33 @@ import com.foosbar.mailsnag.model.Message;
 import com.foosbar.mailsnag.model.Message.Attachment;
 
 public class InlineFilter {
-	
-	private static final Pattern SRC_PATTERN = Pattern.compile("<(IMG)(.+?)(SRC\\s*?=\\s*?)([\"'\\s]{0,1})(cid:(.*?))([\"'\\s>]+)(.+?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
-	
+
+	private static final Pattern SRC_PATTERN = Pattern
+			.compile(
+					"<(IMG)(.+?)(SRC\\s*?=\\s*?)([\"'\\s]{0,1})(cid:(.*?))([\"'\\s>]+)(.+?)>",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+
 	public static String filter(Message message, String content, String path) {
-		
+
 		Matcher m = SRC_PATTERN.matcher(content);
 		StringBuffer sb = new StringBuffer();
-		while(m.find()) {
+		while (m.find()) {
 
 			String contentId = m.group(6);
 			Attachment a = message.getAttachments().get(contentId);
-			
-			if(a == null)
+
+			if (a == null) {
 				continue;
-			
-			m.appendReplacement(sb, "<$1$2$3$4" + path + File.separator + contentId + File.separator + a.getName() + "$7$8>");
-			
+			}
+
+			m.appendReplacement(sb, "<$1$2$3$4" + path + File.separator
+					+ contentId + File.separator + a.getName() + "$7$8>");
+
 		}
 		m.appendTail(sb);
+
+		System.out.println(sb.toString());
+
 		return sb.toString();
 	}
 }
